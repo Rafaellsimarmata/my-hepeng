0a924da972e3c2b996fa870b5a1174e48c81ce9b
Gavriel Sembiring 366 <114407922+Gavriel-Sembiring-366@users.noreply.github.com>
2023-12-13 23:49:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var sidebar = document.getElementById("sidebar");
-    var sidebarCollapse = document.getElementById("sidebarCollapse");
-
-    sidebarCollapse.addEventListener("click", function () {
-        sidebar.classList.toggle("collapsed");
-    });
-});
